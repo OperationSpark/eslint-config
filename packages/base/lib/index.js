b9ca0363446d337ece33dd59d4f1d7267d5461ab
Cain Watson <cainwatson00@gmail.com>
2019-09-24 15:31:07 -0500

    feat: add base eslint config

---
+++ packages/base/lib/index.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    './rules/errors.js',
+    './rules/best-practices.js',
+    './rules/strict.js',
+    './rules/variables.js',
+    './rules/node.js',
+    './rules/style.js',
+    './rules/es6.js',
+  ],
+  env: {
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2019,
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+};
---
+++ packages/base/lib/rules/variables.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    'init-declarations': 2,
+    'no-delete-var': 2,
+    'no-label-var': 2,
+    'no-restricted-globals': 0,
+    'no-shadow': 0,
+    'no-shadow-restricted-names': 2,
+    'no-undef': 0,
+    'no-undef-init': 2,
+    'no-undefined': 2,
+    'no-unused-vars': 2,
+    'no-use-before-define': 2,
+  },
+};
