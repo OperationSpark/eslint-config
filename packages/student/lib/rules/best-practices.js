bb142208d0033241cd66801b9f62d631d548e675
Cain Watson <cainwatson00@gmail.com>
2019-10-12 11:31:38 -0500

    feat: turn off ''no-magic-numbers"

--- packages/student/lib/rules/best-practices.js
+++ packages/student/lib/rules/best-practices.js
@@ -1,7 +1,6 @@
 module.exports = {
   rules: {
     'no-extend-native': 0,
-    'no-magic-numbers': 0,
     'no-param-reassign': 1,
     'no-warning-comments': 0,
     'consistent-return': 1,
--- packages/typescript/lib/rules/best-practices.js
+++ packages/typescript/lib/rules/best-practices.js
@@ -2,8 +2,5 @@ module.exports = {
   rules: {
     'no-empty-function': 0,
     '@typescript-eslint/no-empty-function': 2,
-
-    'no-magic-numbers': 0,
-    '@typescript-eslint/no-magic-numbers': 2,
   },
 };
